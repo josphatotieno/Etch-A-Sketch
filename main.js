1771cae02ba52e079680d90b28e3e6ccf9adbc4b
Josphat Otieno <josphathumphrey5@gmail.com>
2022-07-27 17:00:14 +0300

    Update main.js

--- main.js
+++ main.js
@@ -8,12 +8,14 @@ while(squareDivs > 0) {
         squareDivs--
     }
 
-document.querySelector('#btn').addEventListener('click', () => {
+document.querySelector('#btn1').addEventListener('click', () => {
 
-    const numberOfSquares = prompt('Enter number of squares to create grid');
+    const numberOfSquares = parseInt(prompt('Enter number of squares between 1-100 to create grid'));
 
-    if(numberOfSquares !== '') {
+    if(numberOfSquares > 0 && numberOfSquares <= 100) {
         createGridOfSquares(numberOfSquares);
+    } else {
+        alert('Please enter number between 1-100')
     }   
 });
 
@@ -25,8 +27,10 @@ function createGridOfSquares(squares) {
     while(squareDivs > 0) {
         const div = document.createElement('div');
         div.className = 'square';
-        squaresWrapper.style.gridTemplateColumns = `repeat(${squares},30px)`;
-        
+        squaresWrapper.style.gridTemplateColumns = `repeat(${squares},minmax(0px,1fr))`;
+
+        squaresWrapper.style.gridTemplateRows = `repeat(${squares},minmax(0px,1fr))`;
+
         squaresWrapper.appendChild(div);
 
         squareDivs--;
@@ -37,19 +41,49 @@ const random1 = Math.floor(Math.random() * 256);
 const random2 = Math.floor(Math.random() * 256);
 const random3 = Math.floor(Math.random() * 256);
 
-squaresWrapper.addEventListener('mouseover', (e) => {
-    if(e.target.className === 'square') {
-        e.target.style.backgroundColor = `rgb(${random1},${random2},${random3})`;
-    }
+document.querySelector('#btn2').addEventListener('click', () => {
+    squaresWrapper.addEventListener('mouseover', (e) => {
+        if(e.target.className === 'square') {
+            e.target.style.backgroundColor = `rgb(${random1},${random2},${random3})`;
+        }
+     });
+
+    squaresWrapper.addEventListener('mouseout', (e) => {
+        if(e.target.className === 'square') {
+            e.target.style.backgroundColor = `rgb(${random1},${random2},${random3})`;
+        }
 });
 
-squaresWrapper.addEventListener('mouseout', (e) => {
-    if(e.target.className === 'square') {
-        e.target.style.backgroundColor = `rgb(${random1},${random2},${random3})`;
-    }
-});
+})
 
-console.log(random1,random2,random3);
+// squaresWrapper.addEventListener('mouseover', (e) => {
+//     if(e.target.className === 'square') {
+//         e.target.style.backgroundColor = `rgb(${random1},${random2},${random3})`;
+//     }
+// });
+
+// squaresWrapper.addEventListener('mouseout', (e) => {
+//     if(e.target.className === 'square') {
+//         e.target.style.backgroundColor = `rgb(${random1},${random2},${random3})`;
+//     }
+// });
+
+document.querySelector('#colorpicker').addEventListener('input', (e) => {
+    
+    const colorPicked = e.target.value;
+
+    squaresWrapper.addEventListener('mouseover', (e) => {
+        if(e.target.className === 'square') {
+            e.target.style.backgroundColor = `${colorPicked}`;
+        }
+    });
+    
+    squaresWrapper.addEventListener('mouseout', (e) => {
+        if(e.target.className === 'square') {
+            e.target.style.backgroundColor = `${colorPicked}`;
+        }
+    });
+})
 
 
 
